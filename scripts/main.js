6f36e97ae4d4abb110213f449436f8bf74561ada
isaakboes <isaakboes@gmail.com>
2023-12-03 21:32:46 -0500

    added seeded random and edited index

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
-//init runs when the page loads
+//init begins the creation process for the solar system
 function init(){
-    
+
+}
+//creates the basic framework for the solar system by choosing a centeral body, and amount of planets, astroid belts, comets, material composition, etc
+//also creates div elements for displaying information on the celestial bodies as needed
+function createPlanetaryBodies(){
+    //choosing a centeral body
+
 }
