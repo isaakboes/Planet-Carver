23bf9489b8b5895b277063365c8493ca101eb407
isaakboes <isaakboes@gmail.com>
2023-12-04 19:37:09 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 //init begins the creation process for the solar system
 function init(){
-    Math.seedrandom(document.getElementById("seed").innerHTML);//seeds the random generator based on the seed input
+    Math.seedrandom(document.getElementById("seed").innerHTML+getElementById("eccentricity").innerHTML);//seeds the random generator based on the seed input
 }
 //creates the basic framework for the solar system by choosing a centeral body, and amount of planets, astroid belts, comets, material composition, etc
 //also creates div elements for displaying information on the celestial bodies as needed
