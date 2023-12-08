f4acde554d6522605d651531fc281d88b1e58eab
isaakboes <isaakboes@gmail.com>
2023-12-07 19:33:21 -0500

    json support?

--- scripts/main.js
+++ scripts/main.js
@@ -22,26 +22,41 @@ function print(output){                                     //printing messages 
 }//end print
 var running = false;                            //is the simulation running
 const delay = 100;                              //amount of time (msec) to wait in between steps
+const fetchTimeout = (url, ms, { signal, ...options } = {}) => {            //https://stackoverflow.com/questions/46946380/fetch-api-request-timeout/57888548#57888548
+    const controller = new AbortController();                               //fetching json with a timeout
+    const promise = fetch(url, { signal: controller.signal, ...options });
+    if (signal) signal.addEventListener("abort", () => controller.abort());
+    const timeout = setTimeout(() => controller.abort(), ms);
+    return promise.finally(() => clearTimeout(timeout));
+  };
 /*=======================================================================================================================================================
     init begins the creation process for the solar system, initalizing variables and seeding the random generator.
 =======================================================================================================================================================*/
-function init(){
-    if(!running){                                                                       //if the program is not already running
+async function init(){
+    if(!running){
+        print("Getting Config...");                                                     //prints to get the config json
+        try{
+            let config = JSON.parse(await fetchTimeout("scripts/config.json",5000));            //calling for the json config file and parsing
+        }catch(error){
+            print("Could not access JSON, Exiting...")
+            return;
+        }
+        print("Downloaded JSON...")
+        print("Beginning Generation...")                                                //if the program is not already running
         percentComplete=0;                                                              //resets the completion percentage
         running=true;                                                                   //marks the program as 'running'
         document.getElementById("genButton").classList.add("down");                     //visually shows that the program is running
         print("Using \""+document.getElementById("seed").value+"\" as generation seed");//prints to the custom console
         Math.seedrandom(document.getElementById("seed").value);                         //seeds the random generator based on the seed input
 
-        setTimeout(createPlanetaryBodies(),delay);                                        //waits before continuing to avoid browser freeze
+        setTimeout(createCenteralBodies(),delay);                                      //waits before continuing to avoid browser freeze
     }                                                                                   //if a simulation is already running do nothing
 }
-
 /*=======================================================================================================================================================
-    creates the basic framework for the solar system by choosing a centeral body, and amount of planets, astroid belts, comets, material composition, etc
-    also creates div elements for displaying information on the celestial bodies as needed
+    creates the basic framework for the solar system by choosing a centeral body also creates div elements for displaying information on the celestial 
+    bodies as needed
 =======================================================================================================================================================*/
-function createPlanetaryBodies(){
+function createCenteralBodies(){
     print("Creating celestial objects and choosing solar system composition");
     //choosing a central body
 
@@ -49,7 +64,7 @@ function createPlanetaryBodies(){
     Suggested probabilities for stars:
     
     Numbers are approximate
-    Radius, Mass, Luminosity are relative to sun
+    Radius, Mass, Luminosity are relative to sol
     Type        | Prob | Temp(kK) | Radius | Mass  | Luminosity | Lifetime (yrs) |      Color      |  Common Name  | Activeness |
 
     Main M      | 69.7%| 2.4-3.7  | 0.3    | 0.2   | 0.01       | 200G           | red-orange      | Red Dwarf     | 
@@ -64,5 +79,23 @@ function createPlanetaryBodies(){
     Supergiant  | .4%  | 4-40     | 30-500 | 10-70 | 30k-1M     | 10M            | red to blue     | Supergiant    | 
 
     Black Hole  | .3%  | 0-10000  | 
+    */
+
+}
+function selectElementalMakeup(){
+    //choosing an elemental composition of the system
+    /*
+    [H ]       This periodic table is not useful                        [He]
+    [Li][Be]   But it's fun, so here it is.         [B ][C ][N ][O ][F ][Ne]
+    [Na][Mg]                                        [Al][Si][P ][S ][Cl][Ar]
+    [K ][Ca][Sc][Ti][V ][Cr][Mn][Fe][Co][Ni][Cu][Zn][Ga][Ge][As][Se][Br][Kr]
+    [Rb][Sr][Y ][Zr][Nb][Mo][Tc][Ru][Rh][Pd][Ag][Cd][In][Sn][Sb][Te][I ][Xe]
+    [Cs][Ba][  ][Hf][Ta][W ][Re][Os][Ir][Pt][Au][Hg][Tl][Pb][Bi][Po][At][Rn]
+    [Fr][Ra][  ][Rf][Db][Sg][Bh][Hs][Mt][Ds][Rg][Cn][Nh][Fl][Mc][Lv][Ts][Og]
+            
+            [La][Ce][Pr][Nd][Pm][Sm][Eu][Gd][Tb][Dy][Ho][Er][Tm][Yb][Lu]
+            [Ac][Th][Pa][U ][Np][Pu][Am][Cm][Bk][Cf][Es][Fm][Md][No][Lr]
+
+            element percentages are included in config.json
     */
 }
