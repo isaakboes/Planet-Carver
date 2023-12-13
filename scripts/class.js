f729067d3bb9138c69eadd720f5221940e0422c0
isaakboes <isaakboes@gmail.com>
2023-12-13 10:17:45 -0500

    Update class.js

--- scripts/class.js
+++ scripts/class.js
@@ -1,5 +1,5 @@
 class Object {//the Object class defines any physical body within a star system including but not limited to stars, planets, asteroids, black holes, neutron stars, and comets.
-    constructor(Mass){
+    constructor(mass, volume, makup,){
 
     }
 }
